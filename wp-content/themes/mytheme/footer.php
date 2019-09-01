a230c2cd7c0a6c4a00dd8834411aff8245f70cff
Shiko Tamai <shikou.tamai@gmail.com>
2019-09-01 18:42:04 +0700

    theme preparation

---
+++ wp-content/themes/mytheme/footer.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * My Portfolio WordPress Theme
+ * @author: shiko
+ * @link: https://shiko-tamai.com
+ * @license: http://www.gnu.org/licenses/gpl-2.0.html GPL v2 or later
+ */
+if ( !defined( 'ABSPATH' ) ) exit;
+?>
+
+<?php //フッターで読み込むJavaScript用テンプレート
+  get_template_part('tmp/footer-javascript');
+?>
+
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ wp-content/themes/mytheme/header.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * My Portfolio WordPress Theme
+ * @author: shiko
+ * @link: https://shiko-tamai.com
+ * @license: http://www.gnu.org/licenses/gpl-2.0.html GPL v2 or later
+ */
+if ( !defined( 'ABSPATH' ) ) exit;
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<?php // force Internet Explorer to use the latest rendering engine available ?>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<?php // mobile meta (hooray!) ?>
+<meta name="HandheldFriendly" content="True">
+<meta name="MobileOptimized" content="320">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover"/>
+
+<?php wp_head(); ?>
+
+<?php //ヘッドタグ内挿入用のユーザー用テンプレート
+get_template_part('tmp/head-insert'); ?>
+
+<title>
+<?php   ?>
+<?php wp_title( '|', true, 'right'); // 記事のタイトルやカテゴリ |?>
+<?php bloginfo( 'name' ); // サイト名?>
+</title>
+
+<link rel="stylesheet"
+ href="<?php echo get_stylesheet_uri(); ?>">
+
+</head>
+<?php 	// HEADはここまで BODY開始		?>
+
+<body <?php body_class(); ?>>
+
+<?php //ユーザーカスタマイズ用(headerの前に挿入したいものがあれば以下に記述)
+get_template_part('tmp-user/body-top-insert');
+?>
